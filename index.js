f9d43ac9db89038b5fcfe2a3aaf5d9115a6372a0
eight <eight04@gmail.com>
2017-12-14 18:44:08 +0800

    Implement

---
+++ index.js
@@ -0,0 +1,42 @@
+const orderedObject = require("ordered-object");
+
+function parse(json) {
+  return orderedObject.wrap(JSON.parse(json));
+}
+
+function stringify(target, options = {}) {
+  if (Array.isArray(options)) {
+    options = {order: options};
+  }
+  const {replacer, space, order} = options;
+  if (order) {
+    target = applyOrder(target, order);
+  }
+  return JSON.stringify(target, replacer, space);
+}
+
+function applyOrder(obj, order) {
+  const keys = Object.keys(obj);
+  const cleanOrder = order.map(s => typeof s === "string" ? s : s[0]);
+  const nested = order.filter(Array.isArray);
+  keys.sort(withOrder(cleanOrder));
+  obj = orderedObject.create(obj, keys);
+  for (const [key, order] of nested) {
+    obj[key] = applyOrder(obj[key], order);
+  }
+  return obj;
+}
+
+function withOrder(order) {
+  const key2Index = new Map([...order.entries()].map(([i, k]) => [k, i]));
+  return (a, b) => {
+    const ai = key2Index.get(a);
+    const bi = key2Index.get(b);
+    if (ai != null && bi != null) {
+      return ai - bi;
+    }
+    return 0;
+  };
+}
+
+module.exports = {parse, stringify};
