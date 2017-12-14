8c830170cd380d83ec08ae558c54b54197dd83de
eight <eight04@gmail.com>
2017-12-14 18:44:52 +0800

    Update test

--- test/test.js
+++ test/test.js
@@ -1,7 +1,6 @@
 /* eslint-env mocha */
-
 const assert = require("power-assert");
-const {parse, stringify, create} = require("../index");
+const {parse, stringify} = require("../index");
 
 describe("constraint", () => {
   it("JSON.parse must keep key order", () => {
@@ -18,31 +17,27 @@ describe("constraint", () => {
   });
 });
 
-describe("create", () => {
-  it("customized keys order", () => {
-    const obj = {a: 1, b: 2, c: 3};
-    const ordered = create(obj, ["c", "b", "a"]);
-    assert.deepEqual(Object.values(ordered), [3, 2, 1]);
-  });
-});
-
 describe("parse", () => {
-  const json = '{"b": 1, "a": 2, "c": [1, 2, 3]}';
-  
   it("must parse correctly", () => {
+    const json = '{"b": 1, "a": 2, "c": [1, 2, 3]}';
     assert.deepEqual(parse(json), JSON.parse(json));
   });
   
   it("keep key order", () => {
+    const json = '{"b": 1, "a": 2, "c": [1, 2, 3]}';
     const keys = Object.keys(parse(json));
     assert.deepEqual(keys, ["b", "a", "c"]);
   });
 });
 
 describe("stringify", () => {
-  const obj = create({a: 1, b: 2, c: 3}, ["b", "c", "a"]);
-  
   it("must stringify correctly", () => {
-    assert(stringify(obj) === '{"b":2,"c":3,"a":1}');
+    const json = stringify({a: 1, b: 2, c: 3}, ["b", "c", "a"]);
+    assert(json === '{"b":2,"c":3,"a":1}');
+  });
+  
+  it("nested order", () => {
+    const json = stringify({a: {b: 1, c: 2}}, [["a", ["c", "b"]]]);
+    assert(json === '{"a":{"c":2,"b":1}}');
   });
 });
