d053d61d547a6ddb25153a796e18a42eda713e9f
eight <eight04@gmail.com>
2017-12-23 16:57:52 +0800

    Update ordered-object to 0.2.0

--- index.js
+++ index.js
@@ -16,47 +16,13 @@ function stringify(target, options = {}) {
 }
 
 function applyOrder(obj, order) {
-  const keys = Object.keys(obj);
-  const cleanOrder = insertMissingKeys(
-    keys, order.map(s => typeof s === "string" ? s : s[0]));
+  const cleanOrder = order.map(s => typeof s === "string" ? s : s[0]);
   const nested = order.filter(Array.isArray);
-  keys.sort(withOrder(cleanOrder));
-  obj = orderedObject.create(obj, keys);
+  obj = orderedObject.create(obj, cleanOrder, "keep");
   for (const [key, order] of nested) {
     obj[key] = applyOrder(obj[key], order);
   }
   return obj;
-}
-
-function insertMissingKeys(keys, order) {
-  // remove non-exist keys
-  const keySet = new Set(keys);
-  order = order.filter(k => keySet.has(k));
-  
-  // insert missing keys
-  const orderSet = new Set(order);
-  const output = [];
-  let i = 0;
-  for (const key of keys) {
-    if (orderSet.has(key)) {
-      output.push(order[i++]);
-    } else {
-      output.push(key);
-    }
-  }
-  return output;
-}
-
-function withOrder(order) {
-  const key2Index = new Map([...order.entries()].map(([i, k]) => [k, i]));
-  return (a, b) => {
-    const ai = key2Index.get(a);
-    const bi = key2Index.get(b);
-    if (ai != null && bi != null) {
-      return ai - bi;
-    }
-    return 0;
-  };
 }
 
 module.exports = {parse, stringify};
